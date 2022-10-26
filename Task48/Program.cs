9e88d19110ff18f2d0e737f83298bb484560eb62
43bp2 <43bp@mail.ru>
2022-10-26 20:48:25 +0300

    add sem6 tasks

---
+++ Task48/Program.cs
@@ -0,0 +1,35 @@
+﻿// двумерный массив размера m,n
+// каждый элемент по формуле Amn = m + n
+
+int[,] matrixMN = NewRndArrayMatrix (5, 5);
+PrintMatrix (matrixMN);
+
+
+// метод создания и заполнения массива (m,n) элементами Amn = m + n
+int[,] NewRndArrayMatrix (int rows, int col)
+{
+    var matrix = new int[rows, col]; 
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - определяем кол-во строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 0 - определяем кол-во столбцов
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+// метод печати массива
+void PrintMatrix (int[,] matrix)
+{
+    for (var i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}");
+            else Console.Write($"{matrix[i, j], 4}"); // ,4 - это форматирование - расстояние между числами, чтоб было все ровно
+        }
+        Console.WriteLine("  |");
+    }
+}
---
+++ Task51/Program.cs
@@ -0,0 +1,55 @@
+﻿// двумерный массив
+// сумма элементов, находящихся на главной диагонали
+// с индексами (0,0); (1,1) и т.д.
+
+int[,] arraySum = NewRndArrayMatrix(4, 4, 1, 5);
+PrintMatrix(arraySum);
+Console.WriteLine();
+int sumMainDiagonalMatrix = SumMainDiagonalMatrix (arraySum);
+Console.WriteLine($"Сумма элементов главной диагонали данного массива равна: {sumMainDiagonalMatrix}");
+
+
+// метод суммы элементов главной диагонали двумерного массива
+int SumMainDiagonalMatrix (int[,] matrix)
+{
+    int sum = 0;
+    int i = 0;
+    int j = 0;
+    while (i < matrix.GetLength(0))
+    {
+       sum = sum + matrix[i, j];
+       i++;
+       j++;
+    }
+    return sum;
+}
+
+// метод создания и заполнения массива + и - элементами
+int[,] NewRndArrayMatrix (int rows, int col, int min, int max)
+{
+    var matrix = new int[rows, col]; // var - 
+    var rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) // 0 - определяем кол-во строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 0 - определяем кол-во столбцов
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+// метод печати массива
+void PrintMatrix (int[,] matrix)
+{
+    for (var i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}");
+            else Console.Write($"{matrix[i, j], 4}"); // ,4 - это форматирование - расстояние между числами, чтоб было все ровно
+        }
+        Console.WriteLine("   |");
+    }
+}
